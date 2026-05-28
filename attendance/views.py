fbfbaa907288bd94cf3359154faf9d405838633e
dlwnfh1 <40032667+dlwnfh1@users.noreply.github.com>
2026-05-27 21:58:20 -0400

    fff

--- attendance/views.py
+++ attendance/views.py
@@ -62,13 +62,13 @@ def attendance_pwa_manifest_view(request):
             "theme_color": "#a56a3b",
             "icons": [
                 {
-                    "src": f"{settings.STATIC_URL}core/icons/icon-192.png",
+                    "src": f"{settings.STATIC_URL}core/icons/attendance-check-192.png",
                     "sizes": "192x192",
                     "type": "image/png",
                     "purpose": "any maskable",
                 },
                 {
-                    "src": f"{settings.STATIC_URL}core/icons/icon-512.png",
+                    "src": f"{settings.STATIC_URL}core/icons/attendance-check-512.png",
                     "sizes": "512x512",
                     "type": "image/png",
                     "purpose": "any maskable",
